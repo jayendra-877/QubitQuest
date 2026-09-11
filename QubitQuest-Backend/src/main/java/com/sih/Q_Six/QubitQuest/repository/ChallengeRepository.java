819958c4b38febea5e770c820a053e19d490eee7
DHRUV18UX <dhruvchourey@gmail.com>
2026-09-12 02:02:37 +0530

    nothing changed

--- QubitQuest-Backend/src/main/java/com/sih/Q_Six/QubitQuest/repository/ChallengeRepository.java
+++ QubitQuest-Backend/src/main/java/com/sih/Q_Six/QubitQuest/repository/ChallengeRepository.java
@@ -9,4 +9,5 @@ import java.util.List;
 @Repository
 public interface ChallengeRepository extends JpaRepository<Challenge,Long> {
     List<Challenge> findByMissionIdOrderByOrderNumberAsc(Long missionId);
+
 }
